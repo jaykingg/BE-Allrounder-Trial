776251dd6d4ac686e8b3303ac44ab5e404c2852a
jayking <mojunseo49@naver.com>
2025-08-17 17:24:49 +0900

    init

---
+++ src/main/kotlin/rounderall/allrounder/AllrounderApplication.kt
@@ -0,0 +1,11 @@
+package rounderall.allrounder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllrounderApplication
+
+fun main(args: Array<String>) {
+    runApplication<AllrounderApplication>(*args)
+}
---
+++ src/test/kotlin/rounderall/allrounder/AllrounderApplicationTests.kt
@@ -0,0 +1,13 @@
+package rounderall.allrounder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllrounderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
